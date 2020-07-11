abd8370a68efba7827b9e0120403671af0a25956
Luiz Krzyzaniak <luiz_krzyzaniak@yahoo.com>
2020-07-11 16:10:10 -0300

    add new arquivos

---
+++ src/app/shared/component/star/star.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnChanges, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges{
+    @Input()
+    rating: number = 0;
+    starrWidth: number;
+    
+    ngOnChanges(): void{
+        this.starrWidth = this.rating * 74 /5;
+    }
+}
